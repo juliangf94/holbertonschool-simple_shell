baab445f4d7faf3957e9a733e369e05db09a5860
Gwendal-B <12309@holbertonstudents.com>
2025-12-19 10:36:49 +0100

    add header and fix name

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+/* Prototypes */
+char *_which(char *command);
+char *_getenv(const char *name);
+void execute(char **argv);
+
+#endif /* SHELL_H */
