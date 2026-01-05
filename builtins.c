b2b5d5d40332f0ec8836dff22e6dcb50f175d0e2
Gwendal-B <12309@holbertonstudents.com>
2026-01-05 15:47:09 +0100

    fix exit and other

--- builtins.c
+++ builtins.c
@@ -17,25 +17,19 @@ void print_env(void)
 /**
  * handle_builtins - Gère les commandes intégrées
  * @argv: Tableau des arguments de la commande
- * @line: Pointeur vers la ligne à libérer
  * Return:* Retour:
  *   2 si exit,
  *   1 si env,
  *   0 sinon
  */
-int handle_builtins(char **argv, char *line)
+int handle_builtins(char **argv)
 {
 	if (!argv[0])
 		return (0);
 
 	if (_strcmp(argv[0], "exit") == 0)
 	{
-		int exit_code = 0;
-
-		if (argv[1])
-			exit_code = atoi(argv[1]);
-		free(line);
-		exit(exit_code);
+		return (2);  /* C'est tout ! */
 	}
 	if (_strcmp(argv[0], "env") == 0)
 	{
--- loop.c
+++ loop.c
@@ -16,6 +16,8 @@ void print_prompt(void)
 /**
  * handle_line - traite une ligne de commande
  * @line: ligne à exécuter (doit être mallocée si free line est utilisé)
+ * @prog_name: nom du programme shell (pour les messages d'erreur)
+ * Return: Rien
  */
 void handle_line(char *line, char *prog_name)
 {
@@ -25,7 +27,6 @@ void handle_line(char *line, char *prog_name)
 
 	if (!line)
 		return;
-
 	/* Supprime le '\n' final si présent */
 	if (_strlen(line) > 0 && line[_strlen(line) - 1] == '\n')
 		line[_strlen(line) - 1] = '\0';
@@ -39,7 +40,7 @@ void handle_line(char *line, char *prog_name)
 		return; /* ligne vide */
 
 	/* Built-ins */
-	ret = handle_builtins(argv, line);
+	ret = handle_builtins(argv);
 	if (ret == 1) /* env exécuté */
 		return;
 	if (ret == 2) /* exit demandé */
@@ -47,7 +48,6 @@ void handle_line(char *line, char *prog_name)
 		free(line);
 		exit(0);
 	}
-
 	/* Résolution du chemin */
 	full_path = _resolve_path(argv[0]);
 	if (!full_path)
@@ -57,7 +57,6 @@ void handle_line(char *line, char *prog_name)
 	}
 	/* Exécution de la commande */
 	execute_command(full_path, argv);
-
 	/* Libération si _resolve_path a mallocé */
 	if (full_path != argv[0])
 		free(full_path);
