5bde091adb2c6caeeae39b1fad644c10c0da1657
Gwendal-B <12309@holbertonstudents.com>
2026-01-06 16:17:52 +0100

    fix atoi

--- _atoi.c
+++ _atoi.c
@@ -1,18 +1,9 @@
-#include "shell.h"
-
-/**
- * _atoi - converts a string to an integer
- * @s: string to convert
- * Return: the integer value, or 0 if no valid conversion
- */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
+    int i = 0, result = 0, sign = 1;
     
     if (!s)
-        return (0);
+        return (-1);
     
     if (s[i] == '-')
     {
@@ -20,12 +11,15 @@ int _atoi(char *s)
         i++;
     }
     else if (s[i] == '+')
-    {
         i++;
-    }
-
-    while (s[i] >= '0' && s[i] <= '9')
+    
+    if (!s[i])
+        return (-1);
+    
+    while (s[i])
     {
+        if (s[i] < '0' || s[i] > '9')
+            return (-1);
         result = result * 10 + (s[i] - '0');
         i++;
     }
